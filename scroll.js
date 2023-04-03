340acb3520cb9ddf9a9443117f50f99ee99f9de9
Leslie Swan <42787368+PersonMeetup@users.noreply.github.com>
2023-04-03 12:13:30 -0600

    Commit work done at university

--- scroll.js
+++ scroll.js
@@ -1,48 +1,49 @@
-window.onload = function () {
-	// Sourced from https://webtips.dev/webtips/javascript/how-to-clamp-numbers-in-javascript
-	const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+// Sourced from https://webtips.dev/webtips/javascript/how-to-clamp-numbers-in-javascript
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
-	const theIntro = document.getElementById("intro__the");
-	const theFall = document.getElementById("fall__the");
+// On load, do this...
+function init() {
+	/* Intro Events */
+	enterView({
+		selector: ".intro__collide",
+		offset: 0.35,
+		progress: (el, progress) => {
+			el.style.gap = `${10 * (1 - progress)}em`;
 
-  // On load, do this...
-  function init() {
-		/* Intro Events */
-    enterView({
-      selector: ".intro__collide",
-      offset: 0.4,
-      progress: (el, progress) => {
-        const end = el.querySelector(".intro--end");
-				end.style.transform = `translateY(${(-3.97) * (progress * 100)}px)`;
-      },
-    });
+			const end = el.querySelector(".intro--end");
+			end.style.transform = `translateY(${-0.8 * progress}em)`;
+		},
+	});
 
-		enterView({
-      selector: ".intro__fall",
-      offset: 0,
-			enter: (el) => {
-				theIntro.style.position = "relative";
-			},
-      progress: (el, progress) => {
-				theIntro.style.top = `${(10) * (progress * 100)}px`;
-				theIntro.style.opacity = 1 - progress;
-      },
-    });
+	enterView({
+		selector: ".intro__fall",
+		offset: 0.3495,
+		progress: (el, progress) => {
+			const theIntro = document.getElementById("intro__the");
 
-		/* Fall Events */
-		enterView({
-      selector: ".fall",
-      offset: 1,
-      progress: (el, progress) => {
-				theFall.style.top = `${(-5) + (progress * 65)}em`;
-				if ((progress === 1) || (progress === 0)) {
-					theFall.style.opacity = 0;
-				} else {
-					theFall.style.opacity = 1;
-				}
-      },
-    });
-  }
+			theIntro.style.top = `${10 * (progress * 100)}px`;
+			theIntro.style.opacity = 1 - progress;
+			theIntro.style.rotate = `${40 * progress}deg`;
+		},
+	});
 
-  init();
-};
+	/* Fall Events */
+	enterView({
+		selector: ".fall",
+		offset: 1,
+		progress: (el, progress) => {
+			const theFall = document.getElementById("fall__the");
+
+			theFall.style.top = `${-15 + progress * 160}vh`;
+			theFall.style.rotate = `${-1.5 + 2 * progress}turn`;
+
+			if (progress >= 0.75 || progress === 0) {
+				theFall.style.opacity = 0;
+			} else {
+				theFall.style.opacity = 1;
+			}
+		},
+	});
+}
+
+init();
